8afb86e7dcfbd250fd8aab37ffefac580914edb0
Hosk1702 <nadaquehaceryt@gmail.com>
2025-08-28 23:50:10 -0700

    Add files via upload

---
+++ est datos/cpp/arreglost1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main() {
+    int tamaño;
+
+    cout << "De que tamaño desea su arreglo?";
+    cin >> tamaño;
+
+    int arr[tamaño];
+
+    cout << "Vamos a llenar el arreglo";
+    for (int i=0 ; i<tamaño ; i++){
+        cout << "Ingrese el elemento # " << i+1 << ":";
+        cin >> arr[i];
+    }
+
+    for (int i=0 ; i< tamaño ; i++){
+        cout << arr[i];
+    }
+}
---
+++ est datos/python/arreglo_t1.py
@@ -0,0 +1,13 @@
+print("ingresa la cantidad de elementos:")
+elementos = int(input())
+
+arr = []
+
+print ("llena el arreglo con ", elementos, "cosas!")
+for i in range(elementos):
+    print ("ingrese el elemento numero ", i + 1, ":")
+    cosa = input()
+    
+    arr.append(cosa)
+    
+print("arreglo completo :D :", arr)
