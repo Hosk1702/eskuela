a9a9781d1eab3df3eccd96b941eb80506715bb84
Marco Velazquez <nadaquehaceryt@gmail.com>
2025-08-30 10:42:04 -0700

    INTENTO DE ARREGLARLO

--- est datos/cpp/arreglost1.cpp
+++ est datos/cpp/arreglost1.cpp
@@ -5,19 +5,22 @@ using namespace std;
 int main() {
     int tamaño;
 
-    cout << "De que tamaño desea su arreglo?";
+    cout << "De que tamaño desea su arreglo? endl";
     cin >> tamaño;
+    cin.ignore(); // Limpiar el buffer de entrada
 
-    int arr[tamaño];
+    vector<string> arr(tamaño);
 
-    cout << "Vamos a llenar el arreglo";
+    cout << "Vamos a llenar el arreglo" << "\n\n";
+
     for (int i=0 ; i<tamaño ; i++){
-        cout << "Ingrese el elemento # " << i+1 << ":";
-        cin >> arr[i];
+        cout << "Ingrese el elemento # " << i+1 << ":" << endl;
+        getline(cin >> ws, arr[i]);
     }
 
     for (int i=0 ; i< tamaño ; i++){
-        cout << arr[i];
+        cout << arr[i] << endl;
+        cout << endl;
     }
 
     return 0;
