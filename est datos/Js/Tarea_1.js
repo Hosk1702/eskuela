9f4def9368ea340894e480a65716ae734e4d7fb8
Marco Velazquez <nadaquehaceryt@gmail.com>
2025-08-30 11:24:27 -0700

    Update Tarea_1.js

--- est datos/Js/Tarea_1.js
+++ est datos/Js/Tarea_1.js
@@ -1,1 +1,9 @@
-console.log("Hola Mundo")
+let arr = [1, 2, 3, 4, 5];
+ console.log("desea ver el contenido del array? (si/no)");
+ let respuesta = prompt();
+ if (respuesta = "si")
+    console.log(arr);
+ else 
+    console.log("ok, adiós");
+
+ 
