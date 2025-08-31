f2d12569ae43f7b529cfd40f572b9dc7706a6e85
Marco Velazquez <nadaquehaceryt@gmail.com>
2025-08-30 17:13:13 -0700

    Create Tarea_1.cs

---
+++ est datos/c#/Tarea_1.cs
@@ -0,0 +1,28 @@
+﻿int tam = 0;
+
+Console.WriteLine("Ingresa el tamaño del arreglo: ");
+string? input = Console.ReadLine();
+
+if (!int.TryParse(input, out tam) || tam <= 0)
+{
+    Console.WriteLine("Por favor, ingresa un número entero válido y mayor que cero.");
+    return 1;
+}
+
+dynamic[] array = new dynamic[tam];
+
+Console.WriteLine("Ingresa los elementos del arreglo: ");
+
+for (int i = 0; i < tam; i++)
+{
+    Console.WriteLine("Ingrese el elemento # " + (i + 1) + ":");
+    array[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Los elementos del arreglo son: ");
+for (int i=0; i<tam; i++)
+{
+    Console.WriteLine("El elemento # " + (i + 1) + " es: " + array[i]);
+}
+
+return 0;
