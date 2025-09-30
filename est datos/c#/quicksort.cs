c60a59d3f5c7d0115bd185ae6294f1e5269c26e8
Marco Velazquez <nadaquehaceryt@gmail.com>
2025-09-29 21:38:37 -0700

    Update quicksort.cs

--- est datos/c#/quicksort.cs
+++ est datos/c#/quicksort.cs
@@ -1,34 +1,55 @@
-function cambiar(arr, j, k){
-    let temp = arr[j];
-    arr[j] = arr[k];
-    arr[k] = temp;
-}
+using System;
+using System.ComponentModel.Design;
+class Program{
+    static void cambiar(ref int[] arr, int j, int k) {
+        int temp = arr[j];
+        arr[j] = arr[k];
+        arr[k] = temp;
+    }
+    static int particion(ref int[] arr,int l, int h)
+    {
+        int pivote = arr[h];
+        int j = l - 1;
+        for (int k = 0; k < h; k++)
+        {
+            if (arr[k] <= pivote) {
+                j += 1;
+                cambiar(ref arr, j, k);
+            }
+        }
+        cambiar(ref arr, j+1, h);
+        return j+1;
+    }
 
-function particion(arr,l,h){
-    let pivote = h;
-    let j = l-1;
+    static void quicksort(ref int[] arr, int l, int h) {
+        if (l < h) { 
+            int pivote = particion(ref arr, l, h);
 
-    for(let k = l; k < h; k++){
-        if (arr[k] <= pivote){
-            j += 1;
-            cambiar(arr,j,k);
+            quicksort(ref arr, l, pivote-1);
+            quicksort(ref arr, pivote + 1, h);
+
         }
     }
-    cambiar(arr, j, h);
-    return j-1;
-}
 
-function quicsort(arr,l,h){
-    if (l<h){
-        let pivote = particion(arr,l,h);
 
-        quicsort(arr,l,pivote+1);
-        quicsort(arr,pivote-1,h);    
+    static void imprimir(int[] arr, int tam)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(arr[i] + string.Join(","));
+        }
+    }
+
+    static void Main(string[] args)
+    {
+            int[] arr = {10,7,9,8,1,5};
+            int tam = arr.Length;
+            Console.WriteLine("Arreglo sin ordenar: \n");
+            imprimir(arr,tam);
+            quicksort(ref arr, 0, tam);
+            Console.WriteLine("Arreglo ordenado: \n");
+            imprimir(arr,tam);
+ 
     }
 }
 
-let arr = [10,7,9,8,1,5];
-let tam = arr.length;
-alert("Arreglo sin ordenar: " + arr);
-quicsort(arr,0,tam);
-alert("Arreglo ordenado: " + arr);
