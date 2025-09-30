bdceec1e8b8dd6b4bf9d277df0667222f73fe095
Marco Velazquez <nadaquehaceryt@gmail.com>
2025-09-29 21:48:02 -0700

    Create fibonacci.cpp

---
+++ est datos/pror mirsa/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n){
+    if(n <= 1){
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(int argc, char const *argv[])
+{
+    int num;
+    cout << "Ingrese la cantidad de numeros que desea ver de la sucesion: \n";
+    cin >> num;
+
+    for (int i=0; i <= num;i++){
+        cout << fibonacci(i) << " ";
+    }
+    return 0;
+}
