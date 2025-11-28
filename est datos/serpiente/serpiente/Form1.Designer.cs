b8a96f619a91c3444cbd0f4665269f99d325d02a
Marco Velazquez <nadaquehaceryt@gmail.com>
2025-11-28 10:32:17 -0700

    serpiente correccion

---
+++ est datos/serpiente/serpiente/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace serpiente
+{
+    partial class Snake
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            caja = new PictureBox();
+            gametimer = new System.Windows.Forms.Timer(components);
+            lblScoreTitle = new Label();
+            lblScoreValue = new Label();
+            lblTrapTitle = new Label();
+            lblTrapCountValue = new Label();
+            lblLevelTitle = new Label();
+            lblLevelValue = new Label();
+            lblLeaderboardTitle = new Label();
+            lblLeaderboardValue = new Label();
+            ((System.ComponentModel.ISupportInitialize)caja).BeginInit();
+            SuspendLayout();
+            // 
+            // caja
+            // 
+            caja.BackColor = Color.FromArgb(192, 255, 192);
+            caja.Location = new Point(0, 100);
+            caja.Name = "caja";
+            caja.Size = new Size(1258, 664);
+            caja.TabIndex = 0;
+            caja.TabStop = false;
+            caja.Paint += caja_Paint;
+            // 
+            // gametimer
+            // 
+            gametimer.Tick += gametimer_Tick;
+            // 
+            // lblScoreTitle
+            // 
+            lblScoreTitle.AutoSize = true;
+            lblScoreTitle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblScoreTitle.Location = new Point(12, 9);
+            lblScoreTitle.Name = "lblScoreTitle";
+            lblScoreTitle.Size = new Size(121, 28);
+            lblScoreTitle.TabIndex = 1;
+            lblScoreTitle.Text = "Puntuación:";
+            // 
+            // lblScoreValue
+            // 
+            lblScoreValue.AutoSize = true;
+            lblScoreValue.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblScoreValue.Location = new Point(140, 9);
+            lblScoreValue.Name = "lblScoreValue";
+            lblScoreValue.Size = new Size(24, 28);
+            lblScoreValue.TabIndex = 2;
+            lblScoreValue.Text = "0";
+            // 
+            // lblTrapTitle
+            // 
+            lblTrapTitle.AutoSize = true;
+            lblTrapTitle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblTrapTitle.Location = new Point(12, 37);
+            lblTrapTitle.Name = "lblTrapTitle";
+            lblTrapTitle.Size = new Size(92, 28);
+            lblTrapTitle.TabIndex = 3;
+            lblTrapTitle.Text = "Bombas:";
+            // 
+            // lblTrapCountValue
+            // 
+            lblTrapCountValue.AutoSize = true;
+            lblTrapCountValue.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblTrapCountValue.Location = new Point(140, 37);
+            lblTrapCountValue.Name = "lblTrapCountValue";
+            lblTrapCountValue.Size = new Size(57, 28);
+            lblTrapCountValue.TabIndex = 4;
+            lblTrapCountValue.Text = "0 / 3";
+            // 
+            // lblLevelTitle
+            // 
+            lblLevelTitle.AutoSize = true;
+            lblLevelTitle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblLevelTitle.Location = new Point(12, 65);
+            lblLevelTitle.Name = "lblLevelTitle";
+            lblLevelTitle.Size = new Size(66, 28);
+            lblLevelTitle.TabIndex = 5;
+            lblLevelTitle.Text = "Nivel:";
+            // 
+            // lblLevelValue
+            // 
+            lblLevelValue.AutoSize = true;
+            lblLevelValue.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblLevelValue.Location = new Point(140, 65);
+            lblLevelValue.Name = "lblLevelValue";
+            lblLevelValue.Size = new Size(24, 28);
+            lblLevelValue.TabIndex = 6;
+            lblLevelValue.Text = "1";
+            // 
+            // lblLeaderboardTitle
+            // 
+            lblLeaderboardTitle.AutoSize = true;
+            lblLeaderboardTitle.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            lblLeaderboardTitle.Location = new Point(900, 9);
+            lblLeaderboardTitle.Name = "lblLeaderboardTitle";
+            lblLeaderboardTitle.Size = new Size(224, 28);
+            lblLeaderboardTitle.TabIndex = 7;
+            lblLeaderboardTitle.Text = "Mejores Puntuaciones:";
+            // 
+            // lblLeaderboardValue
+            // 
+            lblLeaderboardValue.AutoSize = true;
+            lblLeaderboardValue.Font = new Font("Segoe UI", 10F);
+            lblLeaderboardValue.Location = new Point(900, 37);
+            lblLeaderboardValue.Name = "lblLeaderboardValue";
+            lblLeaderboardValue.Size = new Size(116, 28);
+            lblLeaderboardValue.TabIndex = 8;
+            lblLeaderboardValue.Text = "(cargando...)";
+            // 
+            // Snake
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1258, 764);
+            Controls.Add(lblLeaderboardValue);
+            Controls.Add(lblLeaderboardTitle);
+            Controls.Add(lblLevelValue);
+            Controls.Add(lblLevelTitle);
+            Controls.Add(lblTrapCountValue);
+            Controls.Add(lblTrapTitle);
+            Controls.Add(lblScoreValue);
+            Controls.Add(lblScoreTitle);
+            Controls.Add(caja);
+            Name = "Snake";
+            Text = "Snake";
+            KeyDown += Snake_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)caja).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox caja;
+        private System.Windows.Forms.Timer gametimer;
+        private Label lblScoreTitle;
+        private Label lblScoreValue;
+        private Label lblTrapTitle;
+        private Label lblTrapCountValue;
+        private Label lblLevelTitle;
+        private Label lblLevelValue;
+        private Label lblLeaderboardTitle;
+        private Label lblLeaderboardValue;
+    }
+}
---
+++ est datos/serpiente/serpiente/Form1.cs
@@ -0,0 +1,468 @@
+﻿using Microsoft.VisualBasic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace serpiente
+{
+    public partial class Snake : Form
+    {
+        private Listas snake = new Listas();
+        private int cuadrado = 20;
+        private string direccion = "derecha";
+        private string siguiente_direccion = "derecha";
+        private bool perder = false;
+
+        private int score = 0;
+        private List<int> highScores = new List<int>();
+        private const string LEADERBOARD_FILE = "leaderboard.txt";
+
+        // Variables de Juego (Niveles, Trampas, Manzanas)
+        private List<Point> traps = new List<Point>();
+        private List<Point> foods = new List<Point>();
+        private const int APPLE_COUNT_ON_SCREEN = 3;
+        private int trapCount = 0;
+        private bool invincible = false;
+
+        private int level = 1;
+        private int appleCount = 0;
+        private const int APPLES_PER_LEVEL = 15;
+        private const int LEVEL_UP_BONUS = 50;
+        private const int BASE_SPEED_MS = 100;
+
+        // Imágenes
+        private Image imgManzana;
+        private Image imgCabezaArriba;
+        private Image imgCabezaAbajo;
+        private Image imgCabezaIzquierda;
+        private Image imgCabezaDerecha;
+        private Image imgColaArriba;
+        private Image imgColaAbajo;
+        private Image imgColaIzquierda;
+        private Image imgColaDerecha;
+        private Image imgCuerpoVertical;
+        private Image imgCuerpoHorizontal;
+        private Image imgEsquinaSupIzquierda;
+        private Image imgEsquinaSupDerecha;
+        private Image imgEsquinaInfIzquierda;
+        private Image imgEsquinaInfDerecha;
+
+        public Snake()
+        {
+            InitializeComponent();
+
+            // Cargar Imágenes
+            imgManzana = Image.FromFile("assets/apple.png");
+            imgCabezaArriba = Image.FromFile("assets/head_up.png");
+            imgCabezaAbajo = Image.FromFile("assets/head_down.png");
+            imgCabezaIzquierda = Image.FromFile("assets/head_left.png");
+            imgCabezaDerecha = Image.FromFile("assets/head_right.png");
+            imgColaArriba = Image.FromFile("assets/tail_up.png");
+            imgColaAbajo = Image.FromFile("assets/tail_down.png");
+            imgColaIzquierda = Image.FromFile("assets/tail_left.png");
+            imgColaDerecha = Image.FromFile("assets/tail_right.png");
+            imgCuerpoVertical = Image.FromFile("assets/body_vertical.png");
+            imgCuerpoHorizontal = Image.FromFile("assets/body_horizontal.png");
+            imgEsquinaSupIzquierda = Image.FromFile("assets/body_topleft.png");
+            imgEsquinaSupDerecha = Image.FromFile("assets/body_topright.png");
+            imgEsquinaInfIzquierda = Image.FromFile("assets/body_bottomleft.png");
+            imgEsquinaInfDerecha = Image.FromFile("assets/body_bottomright.png");
+
+            LoadLeaderboard();
+            UpdateLeaderboardLabel();
+
+            startgame();
+        }
+
+        private void gametimer_Tick(object sender, EventArgs e)
+        {
+            if (perder) return;
+
+            direccion = siguiente_direccion;
+            Point newHead = snake.head.data;
+
+            switch (direccion)
+            {
+                case "derecha": newHead.X += cuadrado; break;
+                case "izquierda": newHead.X -= cuadrado; break;
+                case "arriba": newHead.Y -= cuadrado; break;
+                case "abajo": newHead.Y += cuadrado; break;
+            }
+
+            // Warp (Teletransporte en bordes)
+            int gridWidth = caja.Width / cuadrado;
+            int gridHeight = caja.Height / cuadrado;
+            int maxX = (gridWidth - 1) * cuadrado;
+            int maxY = (gridHeight - 1) * cuadrado;
+
+            if (newHead.X < 0) newHead.X = maxX;
+            else if (newHead.X > maxX) newHead.X = 0;
+            else if (newHead.Y < 0) newHead.Y = maxY;
+            else if (newHead.Y > maxY) newHead.Y = 0;
+
+            snake.begininsert(newHead);
+
+            // Auto-colisión
+            if (!invincible)
+            {
+                Nodo? actseg = snake.head.siguiente;
+                while (actseg != null)
+                {
+                    if (newHead == actseg.data)
+                    {
+                        perdiste();
+                        return;
+                    }
+                    actseg = actseg.siguiente;
+                }
+            }
+
+            // Colisión con Objetos
+            Point? foodHit = null;
+            foreach (Point f in foods)
+            {
+                if (newHead == f) { foodHit = f; break; }
+            }
+
+            Point? trapHit = null;
+            foreach (Point t in traps)
+            {
+                if (newHead == t) { trapHit = t; break; }
+            }
+
+            if (foodHit.HasValue) // Comió manzana
+            {
+                score += 10;
+                appleCount++;
+                UpdateScoreLabel();
+
+                if (appleCount >= APPLES_PER_LEVEL)
+                {
+                    LevelUp();
+                }
+
+                foods.Remove(foodHit.Value);
+                GenerarNuevaComida();
+            }
+            else if (trapHit.HasValue) // Tocó bomba
+            {
+                score -= 10;
+                if (score < 0) score = 0;
+                UpdateScoreLabel();
+
+                if (snake.Length() > 3)
+                {
+                    snake.last_delete();
+                }
+
+                trapCount++;
+                UpdateTrapLabel();
+
+                if (trapCount >= 3)
+                {
+                    perdiste();
+                    return;
+                }
+
+                traps.Remove(trapHit.Value);
+                GenerarNuevaTrampa();
+            }
+            else
+            {
+                snake.last_delete();
+            }
+
+            caja.Invalidate();
+        }
+
+        private void caja_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Nodo? puntero = snake.head;
+            Nodo? previo = null;
+
+            // Dibujar Serpiente
+            while (puntero != null)
+            {
+                Image imgSnake;
+                Nodo? siguiente = puntero.siguiente;
+
+                if (previo == null) // Cabeza
+                {
+                    switch (direccion)
+                    {
+                        case "derecha": imgSnake = imgCabezaDerecha; break;
+                        case "izquierda": imgSnake = imgCabezaIzquierda; break;
+                        case "arriba": imgSnake = imgCabezaArriba; break;
+                        default: imgSnake = imgCabezaAbajo; break;
+                    }
+                }
+                else if (siguiente == null) // Cola
+                {
+                    Point posPrevia = previo.data;
+                    Point posCola = puntero.data;
+
+                    // La cola apunta en la dirección del cuerpo previo
+                    if (posPrevia.X < posCola.X) imgSnake = imgColaDerecha;
+                    else if (posPrevia.X > posCola.X) imgSnake = imgColaIzquierda;
+                    else if (posPrevia.Y < posCola.Y) imgSnake = imgColaAbajo;
+                    else imgSnake = imgColaArriba;
+                }
+                else // Cuerpo
+                {
+                    Point posPrevia = previo.data;
+                    Point posActual = puntero.data;
+                    Point posSiguiente = siguiente.data;
+
+                    if (posPrevia.X == posSiguiente.X) imgSnake = imgCuerpoVertical;
+                    else if (posPrevia.Y == posSiguiente.Y) imgSnake = imgCuerpoHorizontal;
+                    else
+                    {
+                        // Esquinas
+                        if ((posPrevia.Y < posActual.Y && posSiguiente.X < posActual.X) || (posPrevia.X < posActual.X && posSiguiente.Y < posActual.Y))
+                            imgSnake = imgEsquinaSupIzquierda;
+                        else if ((posPrevia.Y < posActual.Y && posSiguiente.X > posActual.X) || (posPrevia.X > posActual.X && posSiguiente.Y < posActual.Y))
+                            imgSnake = imgEsquinaSupDerecha;
+                        else if ((posPrevia.Y > posActual.Y && posSiguiente.X < posActual.X) || (posPrevia.X < posActual.X && posSiguiente.Y > posActual.Y))
+                            imgSnake = imgEsquinaInfIzquierda;
+                        else
+                            imgSnake = imgEsquinaInfDerecha;
+                    }
+                }
+
+                g.DrawImage(imgSnake, puntero.data.X, puntero.data.Y, cuadrado, cuadrado);
+                previo = puntero;
+                puntero = puntero.siguiente;
+            }
+
+            // Dibujar Manzanas
+            foreach (Point f in foods)
+            {
+                g.DrawImage(imgManzana, f.X, f.Y, cuadrado, cuadrado);
+            }
+
+            // Dibujar Trampas
+            foreach (Point t in traps)
+            {
+                g.DrawString("💣", new Font("Segoe UI Emoji", 12), Brushes.Black, t.X, t.Y);
+            }
+        }
+
+        private void Snake_KeyDown(object sender, KeyEventArgs e)
+        {
+            Keys key = e.KeyCode;
+
+            if (key == Keys.Right && direccion != "izquierda") siguiente_direccion = "derecha";
+            else if (key == Keys.Left && direccion != "derecha") siguiente_direccion = "izquierda";
+            else if (key == Keys.Up && direccion != "abajo") siguiente_direccion = "arriba";
+            else if (key == Keys.Down && direccion != "arriba") siguiente_direccion = "abajo";
+
+            // Trucos
+            else if (key == Keys.I) invincible = !invincible;
+            else if (key == Keys.P) { score += 100; UpdateScoreLabel(); }
+        }
+
+        private void startgame()
+        {
+            perder = false;
+            score = 0;
+            trapCount = 0;
+            level = 1;
+            appleCount = 0;
+
+            UpdateScoreLabel();
+            UpdateTrapLabel();
+            UpdateLevelLabel();
+
+            invincible = false;
+            gametimer.Interval = BASE_SPEED_MS;
+
+            direccion = "derecha";
+            siguiente_direccion = "derecha";
+            snake = new Listas();
+            snake.begininsert(new Point(60, 100));
+            snake.begininsert(new Point(80, 100));
+            snake.begininsert(new Point(100, 100));
+
+            // Generación inicial
+            foods.Clear();
+            traps.Clear();
+
+            for (int i = 0; i < APPLE_COUNT_ON_SCREEN; i++) GenerarNuevaComida();
+            GenerarNuevaTrampa();
+
+            gametimer.Start();
+        }
+
+        private Point GetRandomEmptyPoint()
+        {
+            Random rnd = new Random();
+            int x = caja.Width / cuadrado;
+            int y = caja.Height / cuadrado;
+            Point p;
+
+            while (true)
+            {
+                p = new Point(rnd.Next(0, x) * cuadrado, rnd.Next(0, y) * cuadrado);
+
+                // Evitar serpiente
+                bool onSnake = false;
+                Nodo? temp = snake.head;
+                while (temp != null)
+                {
+                    if (p == temp.data) { onSnake = true; break; }
+                    temp = temp.siguiente;
+                }
+                if (onSnake) continue;
+
+                // Evitar otras comidas y trampas
+                if (foods.Contains(p)) continue;
+                if (traps.Contains(p)) continue;
+
+                return p;
+            }
+        }
+
+        private void GenerarNuevaComida()
+        {
+            foods.Add(GetRandomEmptyPoint());
+        }
+
+        private void GenerarNuevaTrampa()
+        {
+            traps.Add(GetRandomEmptyPoint());
+        }
+
+        private void perdiste()
+        {
+            perder = true;
+            gametimer.Stop();
+
+            CheckHighScore();
+            UpdateLeaderboardLabel();
+
+            string message = (trapCount >= 3)
+                ? $"¡Demasiadas bombas! Perdiste.\nPuntuación final: {score}"
+                : $"¡Perdiste! Tu puntuación: {score}";
+
+            MessageBox.Show(message);
+
+            startgame();
+        }
+
+        private void UpdateScoreLabel() { lblScoreValue.Text = score.ToString(); }
+        private void UpdateTrapLabel() { lblTrapCountValue.Text = $"{trapCount} / 3"; }
+        private void UpdateLevelLabel() { lblLevelValue.Text = level.ToString(); }
+
+        private void LevelUp()
+        {
+            level++;
+            appleCount = 0;
+            score += LEVEL_UP_BONUS;
+
+            UpdateScoreLabel();
+            UpdateLevelLabel();
+
+            // Aumentar velocidad (mínimo 40ms)
+            gametimer.Interval = Math.Max(40, BASE_SPEED_MS - (level * 5));
+
+            // Añadir trampa extra
+            GenerarNuevaTrampa();
+        }
+
+        private void LoadLeaderboard()
+        {
+            if (!File.Exists(LEADERBOARD_FILE)) return;
+
+            highScores.Clear();
+            var lines = File.ReadAllLines(LEADERBOARD_FILE);
+            foreach (var line in lines)
+            {
+                if (int.TryParse(line, out int scoreFromFile)) highScores.Add(scoreFromFile);
+            }
+            SortHighScores();
+        }
+
+        private void CheckHighScore()
+        {
+            highScores.Add(score);
+            SortHighScores();
+            if (highScores.Count > 5) highScores = highScores.Take(5).ToList();
+            SaveLeaderboard();
+        }
+
+        private void SaveLeaderboard()
+        {
+            var lines = highScores.Select(s => s.ToString()).ToList();
+            File.WriteAllLines(LEADERBOARD_FILE, lines);
+        }
+
+        private void SortHighScores()
+        {
+            highScores = highScores.OrderByDescending(s => s).ToList();
+        }
+
+        private void UpdateLeaderboardLabel()
+        {
+            StringBuilder sb = new StringBuilder();
+            int count = Math.Min(highScores.Count, 5);
+
+            if (count == 0) sb.AppendLine(" (vacío)");
+            else
+            {
+                for (int i = 0; i < count; i++) sb.AppendLine($"{(i + 1)}. {highScores[i]}");
+            }
+            lblLeaderboardValue.Text = sb.ToString();
+        }
+    }
+
+    public class Nodo
+    {
+        public Point data;
+        public Nodo? siguiente;
+        public Nodo(Point d) { data = d; siguiente = null; }
+    }
+
+    public class Listas
+    {
+        public Nodo? head;
+        public Listas() { head = null; }
+
+        public int Length()
+        {
+            int count = 0;
+            Nodo? temp = head;
+            while (temp != null) { count++; temp = temp.siguiente; }
+            return count;
+        }
+
+        public void begininsert(Point item)
+        {
+            Nodo puntero = new Nodo(item);
+            puntero.siguiente = head;
+            head = puntero;
+        }
+        public void lastinsert(Point item)
+        {
+            Nodo puntero = new Nodo(item);
+            if (head == null) { head = puntero; }
+            else
+            {
+                Nodo temp = head;
+                while (temp.siguiente != null) { temp = temp.siguiente; }
+                temp.siguiente = puntero;
+            }
+        }
+        public void last_delete()
+        {
+            if (head == null) { return; }
+            else if (head.siguiente == null) { head = null; }
+            else
+            {
+                Nodo temp = head;
+                while (temp.siguiente.siguiente != null) { temp = temp.siguiente; }
+                temp.siguiente = null;
+            }
+        }
+    }
+}
